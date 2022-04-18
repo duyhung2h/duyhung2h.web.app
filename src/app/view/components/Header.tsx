7d4936679c4d203d768e92782425214207ae69b1
Hung. Nguyen Duy - CMC Global DU3.11 <ndhung7@cmcglobal.vn>
2022-04-18 17:29:15 +0700

    Thay AuthContext.Consumer thanh ctx = useContext(AuthContext)

--- src/app/view/components/Header.tsx
+++ src/app/view/components/Header.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useContext } from "react";
 import { NavLink } from "react-router-dom";
 import classes from "../../../assets/scss/index.module.scss";
 import AuthContext, {
@@ -20,58 +20,56 @@ const MainHeader = (props: any) => {
     props.onLogin(false);
     logout();
   };
+  const ctx = useContext(AuthContext);
+
+  console.log(
+    "AuthContext.Consumer(ctx) => ctx.isLoggedIn = " + ctx.isLoggedIn
+  );
   return (
     <header className={classes.header}>
       <nav>
-        <AuthContext.Consumer>
-          {(ctx) => {
-            console.log(
-              "AuthContext.Consumer(ctx) => ctx.isLoggedIn = " + ctx.isLoggedIn
-            );
-            return (
-              <ul>
-                <li>
-                  <NavLink activeClassName={classes.active} to="/home">
-                    Home
-                  </NavLink>
-                </li>
-                <li>
-                  <NavLink activeClassName={classes.active} to="/examples">
-                    Examples
-                  </NavLink>
-                </li>
+        <ul>
+          <li>
+            <NavLink activeClassName={classes.active} to="/home">
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink activeClassName={classes.active} to="/examples">
+              Examples
+            </NavLink>
+          </li>
 
-                {!ctx.isLoggedIn && (
-                  <li className={classes.login__hidden + ` ml-3`}>
-                    <a>
-                      <i className="fa fa-user mr-1"></i>
-                      Login
-                    </a>
-                    <LoginComponent onLogin={loginHandler} />
-                  </li>
-                )}
-                {ctx.isLoggedIn && (
-                  <li className={classes.login__hidden + ` ml-3 white-text`}>
-                    <i className="fa fa-user mr-1"></i>
-                    Welcome,{" "}
-                    {true && <span>{getUserLocalstorage()._username}</span>} !
-                  </li>
-                )}
-                {ctx.isLoggedIn && (
-                  <li
-                    onClick={logoutHandler}
-                    className={classes.login__hidden + ` ml-3`}
-                  >
-                    <a>
-                      <i className="fa fa-sign-out mr-1"></i>
-                      Logout
-                    </a>
-                  </li>
-                )}
-              </ul>
-            );
-          }}
-        </AuthContext.Consumer>
+          {!ctx.isLoggedIn && (
+            <li className={classes.login__hidden + ` ml-3`}>
+              <a>
+                <i className="fa fa-user mr-1"></i>
+                Login
+              </a>
+              <LoginComponent onLogin={loginHandler} />
+            </li>
+          )}
+          {ctx.isLoggedIn && (
+            <li className={classes.login__hidden + ` ml-3 white-text`}>
+              <i className="fa fa-user mr-1"></i>
+              Welcome, {true && (
+                <span>{getUserLocalstorage()._username}</span>
+              )}{" "}
+              !
+            </li>
+          )}
+          {ctx.isLoggedIn && (
+            <li
+              onClick={logoutHandler}
+              className={classes.login__hidden + ` ml-3`}
+            >
+              <a>
+                <i className="fa fa-sign-out mr-1"></i>
+                Logout
+              </a>
+            </li>
+          )}
+        </ul>
       </nav>
     </header>
   );
