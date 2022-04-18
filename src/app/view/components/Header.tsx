4df8432f22832c7f57f5f2dc084a4e7d4cb3b9d2
Hung. Nguyen Duy - CMC Global DU3.11 <ndhung7@cmcglobal.vn>
2022-04-18 16:43:16 +0700

    add thanh cong React context cho component login (AuthContext) va get user tu localStorage o trong auth service

--- src/app/view/components/Header.tsx
+++ src/app/view/components/Header.tsx
@@ -1,74 +1,80 @@
+import React from "react";
 import { NavLink } from "react-router-dom";
-import React, { useEffect, useState } from "react";
 import classes from "../../../assets/scss/index.module.scss";
+import AuthContext, {
+  getUserLocalstorage,
+  login,
+  logout,
+} from "../../db/auth.service";
 import LoginComponent from "./LoginComponent";
-import { User } from "../../model/User";
-import { login, logout } from "../../db/auth.service";
 
 const MainHeader = (props: any) => {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-  useEffect(() => {
-    try {
-      var user = new User(JSON.parse(localStorage["user"]).userId, JSON.parse(localStorage["user"]).username, JSON.parse(localStorage["user"]).password)
-      if(user){
-        setIsLoggedIn(true)
-      }
-    } catch (error) {
-      console.log(error);
-    }
-  }, []);
-
   const loginHandler = (username: string, password: string) => {
     // We should of course check email and password
     // But it's just a dummy/ demo anyways
-    setIsLoggedIn(true);
-    login(username, password)
+    props.onLogin(true);
+    login(username, password);
   };
 
   const logoutHandler = () => {
-    setIsLoggedIn(false);
-    logout()
+    props.onLogin(false);
+    logout();
   };
   return (
     <header className={classes.header}>
       <nav>
-        <ul>
-          <li>
-            <NavLink activeClassName={classes.active} to="/home">
-              Home
-            </NavLink>
-          </li>
-          <li>
-            <NavLink activeClassName={classes.active} to="/examples">
-              Examples
-            </NavLink>
-          </li>
-          {!isLoggedIn && (
-            <li className={classes.login__hidden + ` ml-3`}>
-              <a>
-                <i className="fa fa-user mr-1"></i>
-                Login
-              </a>
-              <LoginComponent onLogin={loginHandler} />
-            </li>
-          )}
-          {isLoggedIn && (
-            <li className={classes.login__hidden + ` ml-3 white-text`}>
-                <i className="fa fa-user mr-1"></i>
-                Welcome, {true && (<span>{JSON.parse(localStorage["user"])._username}</span>)} !
-            </li>
-          )}
-          {isLoggedIn && (
-            <li onClick={logoutHandler} className={classes.login__hidden + ` ml-3`}>
-              <a>
-                <i className="fa fa-sign-out mr-1"></i>
-                Logout
-              </a>
-            </li>
-          )}
-        </ul>
+        <AuthContext.Consumer>
+          {(ctx) => {
+            console.log(
+              "AuthContext.Consumer(ctx) => ctx.isLoggedIn = " + ctx.isLoggedIn
+            );
+            return (
+              <ul>
+                <li>
+                  <NavLink activeClassName={classes.active} to="/home">
+                    Home
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink activeClassName={classes.active} to="/examples">
+                    Examples
+                  </NavLink>
+                </li>
+
+                {!ctx.isLoggedIn && (
+                  <li className={classes.login__hidden + ` ml-3`}>
+                    <a>
+                      <i className="fa fa-user mr-1"></i>
+                      Login
+                    </a>
+                    <LoginComponent onLogin={loginHandler} />
+                  </li>
+                )}
+                {ctx.isLoggedIn && (
+                  <li className={classes.login__hidden + ` ml-3 white-text`}>
+                    <i className="fa fa-user mr-1"></i>
+                    Welcome,{" "}
+                    {true && <span>{getUserLocalstorage()._username}</span>} !
+                  </li>
+                )}
+                {ctx.isLoggedIn && (
+                  <li
+                    onClick={logoutHandler}
+                    className={classes.login__hidden + ` ml-3`}
+                  >
+                    <a>
+                      <i className="fa fa-sign-out mr-1"></i>
+                      Logout
+                    </a>
+                  </li>
+                )}
+              </ul>
+            );
+          }}
+        </AuthContext.Consumer>
       </nav>
     </header>
   );
 };
+
 export default MainHeader;
