2b5f343ad678cd8f80d29bcb2ea4f658281612b8
duyhung2h <40296674+duyhung2h@users.noreply.github.com>
2022-04-21 01:49:41 +0700

    stuffs

--- src/app/view/pages/SecretPage.tsx
+++ src/app/view/pages/SecretPage.tsx
@@ -8,9 +8,9 @@ const SecretPage = () => {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
   /**
-   * 
-   * 
-   * @function useCallback(): hook to avoid infinite loop
+   *
+   *
+   * @function useCallback(): hook to avoid infinite loop by using useState after useEffect
    * @function fetch(): fetch data through API, first argument string to the API address, second argument for adding various options
    * @function then(): promise function to handle function after a request is finished
    */
@@ -18,7 +18,9 @@ const SecretPage = () => {
     setIsLoading(true);
     setError(null);
     try {
-      const response = await fetch("https://swapi.dev/api/films/");
+      const response = await fetch("https://swapi.dev/api/films/",{
+        method: 'GET'
+      });
       if (!response.ok) {
         throw new Error("Something went wrong!");
       }
@@ -40,37 +42,43 @@ const SecretPage = () => {
     }
     setIsLoading(false);
   }, []);
-  
+
   // initial fetch movie on site load
   useEffect(() => {
     fetchMoviesHandler();
   }, [fetchMoviesHandler]);
 
-  function addMovieHandler(movie: any) {
+  const addMovieHandler = async(movie: any) => {
     console.log(movie);
     setIsLoading(true);
     setError(null);
-    // try {
-    //   const response = await fetch("https://swapi.dev/api/films/");
-    //   if (!response.ok) {
-    //     throw new Error("Something went wrong!");
-    //   }
+    try {
+      const response = await fetch("https://react-http-6b4a6.firebaseio.com/movies.json",{
+        method: 'POST',
+        body: JSON.stringify(movie),
+        headers:{
+          'Content-Type': 'application/json'
+        }
+      });
+      if (!response.ok) {
+        throw new Error("Something went wrong!");
+      }
 
-    //   const data = await response.json();
-    //   console.log(data);
+      const data = await response.json();
+      console.log(data);
 
-    //   const transformedMovies = data.results.map((movieData: any) => {
-    //     return {
-    //       id: movieData.episode_id,
-    //       title: movieData.title,
-    //       openingText: movieData.opening_crawl,
-    //       releaseDate: movieData.release_date,
-    //     };
-    //   });
-    //   setMovies(transformedMovies);
-    // } catch (error: any) {
-    //   setError(error["message"]);
-    // }
+      const transformedMovies = data.results.map((movieData: any) => {
+        return {
+          id: movieData.episode_id,
+          title: movieData.title,
+          openingText: movieData.opening_crawl,
+          releaseDate: movieData.release_date,
+        };
+      });
+      setMovies(transformedMovies);
+    } catch (error: any) {
+      setError(error["message"]);
+    }
     setIsLoading(false);
   }
   let content = <p>Found no movies.</p>;
