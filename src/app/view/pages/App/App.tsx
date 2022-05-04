92fd8003a1094b50ce3dc6c073adb80ddf1fbe27
duyhung2h <40296674+duyhung2h@users.noreply.github.com>
2022-05-05 02:52:50 +0700

    more

--- src/app/view/pages/App/App.tsx
+++ src/app/view/pages/App/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
-import { Redirect, Route } from "react-router-dom";
+import { NavLink, Redirect, Route } from "react-router-dom";
 import { AuthContextProvider } from "../../../db/auth.service";
+import classes from "../../../../assets/scss/index.module.scss";
 import MainHeader from "../../components/Header";
 import GetExamplePage from "../ExamplePage";
 import SecretPage from "../SecretPage";
@@ -9,27 +10,27 @@ import Home from "./../home";
 function App() {
   return (
     <React.Fragment>
-      <p>Test if works App</p>
+      {/* <p>Test if works App</p> */}
       <AuthContextProvider>
-        Test if works AuthContext
+        {/* Test if works AuthContext */}
         <MainHeader>
-          <p>Test if works MainHeader</p>
+          {/* <p>Test if works MainHeader</p> */}
         </MainHeader>
         <div className="p-5">
           <Route exact path={"/"}>
-            <p>Test if works App /</p>
+            {/* <p>Test if works App /</p> */}
             <Redirect to="/home" />
           </Route>
           <Route exact path={"/home"}>
-            <p>Test if works App /home</p>
+            <NavLink to="/">Main</NavLink> {'>'} <NavLink activeClassName={classes.active} to="/home">Homepage</NavLink> 
             <Home />
           </Route>
           <Route exact path={"/examples"}>
-            <p>Test if works App /examples</p>
+            <NavLink to="/">Main</NavLink> {'>'} <NavLink activeClassName={classes.active} to="/examples">Example Page</NavLink> 
             <GetExamplePage />
           </Route>
           <Route exact path={"/secret"}>
-            <p>Test if works App /secret</p>
+            <NavLink to="/">Main</NavLink> {'>'} <NavLink activeClassName={classes.active} to="/secret">Secret Page</NavLink> 
             <SecretPage />
           </Route>
         </div>
