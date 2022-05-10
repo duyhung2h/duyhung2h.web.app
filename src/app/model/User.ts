b79aff1be76d532954567abc3b95c10d3aac397e
duyhung2h <duyhung2h@gmail.com>
2022-05-10 17:34:22 +0700

    botchered authentication login

--- src/app/model/User.ts
+++ src/app/model/User.ts
@@ -2,15 +2,18 @@ export class User{
     private _userId: number;
     private _username: string;
     private _password: string;
+    private _token: string;
 
-	constructor(userId: number, username: string, password: string) {
+
+	constructor(userId: number, username: string, password: string, token: string) {
 		this._userId = userId;
 		this._username = username;
 		this._password = password;
+		this._token = token;
 	}
 
     /**
-     * Getter customerId
+     * Getter userId
      * @return {number}
      */
 	public get userId(): number {
@@ -34,7 +37,15 @@ export class User{
 	}
 
     /**
-     * Setter customerId
+     * Getter token
+     * @return {string}
+     */
+	public get token(): string {
+		return this._token;
+	}
+
+    /**
+     * Setter userId
      * @param {number} value
      */
 	public set userId(value: number) {
@@ -57,5 +68,13 @@ export class User{
 		this._password = value;
 	}
 
+    /**
+     * Setter token
+     * @param {string} value
+     */
+	public set token(value: string) {
+		this._token = value;
+	}
+    
     
 }
