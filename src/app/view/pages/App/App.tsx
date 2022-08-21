0af88380737553a66d3b0e41514330a12ff12819
duyhung2h <duyhung2h@gmail.com>
2022-08-21 11:42:52 +0700

    added seo

--- src/app/view/pages/App/App.tsx
+++ src/app/view/pages/App/App.tsx
@@ -4,6 +4,10 @@ import { React } from "../../../../index";
 import { initialState, reducer } from "../../../db/reducer/reducer";
 import { displayAlertSuccessPopup } from "../../small_components/AlertInfoPopup";
 import AppContextProvider from "./AppContextProvider";
+import { Helmet } from "react-helmet";
+import Favicon from "react-favicon";
+// import Favicon from "../../../../assets/images/icon/app/favicon.ico"
+
 export var AppContext = createContext<any>({});
 
 function App() {
@@ -20,6 +24,30 @@ function App() {
   // const notification = useSelector((state: RootState) => state.notification);
   return (
     <React.Fragment>
+      <Helmet>
+        {/* metadata */}
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta
+          name="description"
+          content="This is my haven, where I can express myself, post my online tutorials and information about my Youtube channel."
+        />
+        <meta
+          name="robots"
+          content="Age of Empires 2 aoe2 personal website duyhung2h nguyễn duy hưng"
+        />
+        <meta property="og:url" content="duyhung2h.web.app" />
+        <meta property="og:site_name" content="duyhung2h's personal website" />
+        <meta property="og:locale" content="en_US" />
+        <meta property="og:type" content="article" />
+        <meta property="fb:app_id" content="ID_APP_FACEBOOK" />
+        <script
+          src="https://kit.fontawesome.com/fa32aa4cba.js"
+          crossOrigin="anonymous"
+        ></script>
+        <Favicon url="../../../../assets/images/icon/app/favicon.ico"></Favicon>
+        <title>duyhung2h's personal website</title>
+      </Helmet>
       <ThemeProvider theme={currentTheme}>
         <AppContext.Provider value={{ ...state, dispatch }}>
           <AppContextProvider></AppContextProvider>
